f83aabc5d3742ebbf2429eab24a582f9182fc012
Semen Bevzuk <semen.bevzuk@gmail.com>
2016-11-07 23:44:26 +0300

    Изменил схему обмена данными, получил ускорение в 7 раз при перемножении матриц 1000x1000

--- MPI_2_18/main.cpp
+++ MPI_2_18/main.cpp
@@ -120,6 +120,7 @@ int main(int argc, char* argv[]) {
 	int *Cmatrix = NULL;
 	int *Abuff = NULL;
 	int *Bbuff = NULL;
+	int* result_Multiplication = NULL;
 	int thread_count, rank;
 	int dataSize, bufferSize;
 	int remainingData;
@@ -132,15 +133,14 @@ int main(int argc, char* argv[]) {
 	else {
 		printf_s("Error with argv: argc!=4\n");
 	}
-	int* result_Multiplication = (int*)malloc(l * sizeof(int));
 	int* result_matrix = (int*)malloc(m*l* sizeof(int));
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &thread_count);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	MPI_Datatype StructTypeColomn;
 	int count = n;
+	MPI_Datatype StructTypeColomn;
 	int *blocklens = new int[n];
 	MPI_Aint *indices = new MPI_Aint[n];
 	MPI_Datatype* oldtypes = new MPI_Datatype[n];
@@ -153,20 +153,37 @@ int main(int argc, char* argv[]) {
 	MPI_Type_create_struct(count, blocklens, indices, oldtypes, &StructTypeColomn);
 	MPI_Type_commit(&StructTypeColomn);
  
-	dataSize = n;
-	bufferSize = dataSize;
+	//dataSize = n;
+	//bufferSize = dataSize;
 	int data_slice = m / (thread_count - 1);
 	//printf_s("data_slice=%d\n", data_slice);
 	int delta_data = m - (thread_count - 1)*data_slice;
 	//printf_s("delta_data=%d\n", delta_data);
 
+	int* Elements_in_thread = new int[thread_count];
+	for (i = 1; i < thread_count - 1; i++)
+	{
+		Elements_in_thread[i] = data_slice;
+	}
+	Elements_in_thread[0] = 0;
+	Elements_in_thread[thread_count - 1] = data_slice + delta_data;
+
+	dataSize = n;
+	bufferSize = dataSize;
+	if (rank != 0)
+	{
+		bufferSize = dataSize*Elements_in_thread[rank];
+		result_Multiplication = (int*)malloc(Elements_in_thread[rank] * l * sizeof(int));
+	}
 	Abuff = new int[bufferSize];
-	Bbuff = new int[bufferSize];
+	Bbuff = new int[n];
+	
 
 	if (rank == 0) {
 		Amatrix = CreateAndFillMatrixVector(m, n);
 		Bmatrix = CreateAndFillMatrixVector(n, l);
-		if (m*n <= 100 && n*l<=100) {
+		PrintVector(Elements_in_thread, thread_count);
+		if (m*n <= 50 && n*l<=50) {
 			printf_s("Matrix A:\n");
 			PrintMatrixVector(Amatrix, m, n);
 			printf_s("\n");
@@ -179,40 +196,24 @@ int main(int argc, char* argv[]) {
 		time2 = MPI_Wtime();
 		delta_time_consistent = time2 - time1;
 		time1 = MPI_Wtime();
-		int *temp_start_Amatrix = Amatrix; //рассылка строк
-		int count = 0;
-		for (count = 0; count < data_slice; count++) {
-			for (i = 1; i < thread_count; i++) {
-				MPI_Send(temp_start_Amatrix, n, MPI_INT, i, 0, MPI_COMM_WORLD);
-				temp_start_Amatrix = temp_start_Amatrix + n;
-				MPI_Send(Bmatrix, 1, StructTypeColomn, i, 0, MPI_COMM_WORLD);
-			}
-			for (i = 1; i < l; i++) {
-				for (j = 1; j < thread_count; j++) {
-					MPI_Send(Bmatrix + i, 1, StructTypeColomn, j, 0, MPI_COMM_WORLD);
-				}
-			}
-			for (i = 1; i < thread_count; i++) {
-				MPI_Recv(result_matrix + count*l*(thread_count - 1) + l*(i - 1), l, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
-			}
-		}
-		if (delta_data!=0)
-		{
-			for (i = 1; i <= delta_data; i++) {
-				MPI_Send(temp_start_Amatrix, n, MPI_INT, i, 0, MPI_COMM_WORLD);
-				temp_start_Amatrix = temp_start_Amatrix + n;
-				MPI_Send(Bmatrix, 1, StructTypeColomn, i, 0, MPI_COMM_WORLD);
-			}
-			for (i = 1; i < l; i++) {
-				for (j = 1; j <= delta_data; j++) {
-					MPI_Send(Bmatrix + i, 1, StructTypeColomn, j, 0, MPI_COMM_WORLD);
-				}
-			}
-			for (i = 1; i <= delta_data; i++) {
-				MPI_Recv(result_matrix + count*l*(thread_count - 1) + l*(i - 1), l, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
-			}
-		}
 
+		int *temp_start_Amatrix = Amatrix; //рассылка строк
+
+		for (i = 1; i < thread_count; i++) {
+			MPI_Send(temp_start_Amatrix, n*Elements_in_thread[i], MPI_INT, i, 0, MPI_COMM_WORLD);
+			temp_start_Amatrix = temp_start_Amatrix + n*Elements_in_thread[i];
+			MPI_Send(Bmatrix, 1, StructTypeColomn, i, 0, MPI_COMM_WORLD);
+		}
+		for (i = 1; i < l; i++) {
+			for (j = 1; j < thread_count; j++) {
+				MPI_Send(Bmatrix + i, 1, StructTypeColomn, j, 0, MPI_COMM_WORLD);
+			}
+		}
+		int* temp_resukt_matrix = result_matrix;
+		for (i = 1; i < thread_count; i++) {
+			MPI_Recv(temp_resukt_matrix, Elements_in_thread[i] * l, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+			temp_resukt_matrix = temp_resukt_matrix + Elements_in_thread[i] * l;
+		}
 
 		//PrintMatrixVector(result_matrix, m, l);
 		time2 = MPI_Wtime();
@@ -230,32 +231,25 @@ int main(int argc, char* argv[]) {
 		printf_s("Acceleration(parallel):  %f\n", (delta_time_consistent / delta_time_parallel));
 	}
 	if(rank != 0) {
-		for (int count = 0; count < data_slice; count++) {
 			MPI_Recv(Abuff, bufferSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 			MPI_Recv(Bbuff, n, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			//PrintVector(Bbuff, n);
-			result_Multiplication[0] = ScalarMultiplication(Abuff, Bbuff, n);
+			//PrintVector(Abuff, bufferSize);
+			for (i = 0; i < Elements_in_thread[rank]; i++)
+			{
+				result_Multiplication[0+l*i] = ScalarMultiplication(Abuff + n*i, Bbuff, n);
+			}
+			//PrintVector(result_Multiplication, Elements_in_thread[rank] * l);
+
 			for (i = 1; i < l; i++) {
 				MPI_Recv(Bbuff, n, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 				//PrintVector(Bbuff, n);
-				result_Multiplication[i] = ScalarMultiplication(Abuff, Bbuff, n);
+				for (j = 0; j < Elements_in_thread[rank]; j++) {
+					result_Multiplication[i + l*j] = ScalarMultiplication(Abuff + n*j, Bbuff, n);
+				}
 			}
+			//PrintVector(result_Multiplication, Elements_in_thread[rank] * l);
 			//printf_s("rank = %d, c1 = %d, c2 = %d, c3 = %d, c4 = %d\n", rank, result_Multiplication[0], result_Multiplication[1], result_Multiplication[2], result_Multiplication[3]);
-			MPI_Send(result_Multiplication, l, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		}
-		if ((delta_data != 0) && (rank<=delta_data)) {
-			MPI_Recv(Abuff, bufferSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-			MPI_Recv(Bbuff, n, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			//PrintVector(Bbuff, n);
-			result_Multiplication[0] = ScalarMultiplication(Abuff, Bbuff, n);
-			for (i = 1; i < l; i++) {
-				MPI_Recv(Bbuff, n, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-				//PrintVector(Bbuff, n);
-				result_Multiplication[i] = ScalarMultiplication(Abuff, Bbuff, n);
-			}
-			//printf_s("rank = %d, c1 = %d, c2 = %d, c3 = %d, c4 = %d\n", rank, result_Multiplication[0], result_Multiplication[1], result_Multiplication[2], result_Multiplication[3]);
-			MPI_Send(result_Multiplication, l, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		}
+			MPI_Send(result_Multiplication, Elements_in_thread[rank] * l, MPI_INT, 0, 0, MPI_COMM_WORLD);
 	}
 
 	MPI_Type_free(&StructTypeColomn);
